a5ab28717e2e94218142ad0ebf7d41effc7aa695
jpfcabral <jpfcabral@gmail.com>
2021-11-06 11:25:07 -0300

    many-to-many-association

--- src/main/java/com/joaopedro/cursomc/domain/Categoria.java
+++ src/main/java/com/joaopedro/cursomc/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.joaopedro.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -17,14 +20,26 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos =  new ArrayList<>();
+
 	public Categoria(){
 		
 	}
-	
+
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
+	}
+
+		
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	public Integer getId() {
